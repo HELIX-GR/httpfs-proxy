112ec0a4456c2d446b4c19f18b1fcc57a7c9d00c
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2019-10-05 02:00:16 +0300

    Support configuration for remote addresses for administrator

--- src/main/java/gr/helix/httpfsproxy/config/SecurityConfiguration.java
+++ src/main/java/gr/helix/httpfsproxy/config/SecurityConfiguration.java
@@ -2,8 +2,13 @@ package gr.helix.httpfsproxy.config;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,20 +24,31 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.google.common.net.InetAddresses;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
-    @Value("${gr.helix.httpfsproxy.admin.admin-password}")
-    String adminPassword;
+    private static final Pattern CIDR_NETWORK_PATTERN = 
+        Pattern.compile("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})/(\\d{1,2})");
     
+    @Value("${gr.helix.httpfsproxy.admin.admin-password}")
+    private String adminPassword;
+    
+    @Value("${gr.helix.httpfsproxy.admin.remote-addresses:127.0.0.1/8}")
+    private String[] adminRemoteAddresses; 
+        
     @Autowired
-    UserDetailsService userService;
+    private UserDetailsService userService;
+    
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     
     private RequestMatcher csrfProtectionRequestMatcher = new RequestMatcher()
     {
@@ -47,6 +63,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
             return !allowedMethods.contains(request.getMethod()) && !excludePathMatcher.matches(request);
         }
     };
+    
+    @PostConstruct
+    private void checkAdminRemoteAddresses()
+    {
+        for (String r: this.adminRemoteAddresses) {
+            Matcher matcher = CIDR_NETWORK_PATTERN.matcher(r);
+            if (!matcher.matches() || !InetAddresses.isInetAddress(matcher.group(1)) ||
+                    Integer.parseUnsignedInt(matcher.group(2)) > 32) {
+                throw new IllegalStateException("address is not a CIDR network: " + r);
+            }
+        }
+    }
     
     @Override
     public void configure(WebSecurity security) throws Exception
@@ -63,7 +91,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
                 .password("{noop}" + adminPassword);
         
         builder.userDetailsService(userService)
-            .passwordEncoder(new BCryptPasswordEncoder());
+            .passwordEncoder(passwordEncoder);
         
         builder.eraseCredentials(true);
     }
@@ -71,16 +99,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity security) throws Exception
     {        
+        final String userAccessExpression = "hasAuthority('USER')";
+        
+        final String adminAccessExpression = "hasAuthority('ADMIN') and " +
+            Arrays.stream(adminRemoteAddresses).map(r -> "hasIpAddress(" + "'" + r + "'" + ")")
+                .collect(Collectors.joining(" or ", "(", ")"));
+        
         security.authorizeRequests()
             .antMatchers(
                     "/index", "/", "/about")
                 .permitAll()
             .antMatchers(
                     "/users/me", "/files/**")
-                .access("hasAuthority('USER')")
+                .access(userAccessExpression)
             .antMatchers(
                     "/admin/**")
-                .access("hasAuthority('ADMIN') and hasIpAddress('127.0.0.1/8')");
+                .access(adminAccessExpression);
         
         security.sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.NEVER);
