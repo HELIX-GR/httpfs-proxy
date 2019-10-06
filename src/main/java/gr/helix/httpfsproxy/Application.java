ab245fe4f8fa96060ded806c42cfdd6f49613ae6
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2019-10-06 10:42:59 +0300

    Support deploying as a WAR application

--- src/main/java/gr/helix/httpfsproxy/Application.java
+++ src/main/java/gr/helix/httpfsproxy/Application.java
@@ -2,14 +2,27 @@ package gr.helix.httpfsproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    /**
+     * Used when packaging as a JAR application
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	/**
+     * Used when packaging as a WAR application
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(Application.class);
+}
 }
